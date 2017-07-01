b67a074a780100cc07714ab86628ca583de4a438
George Petculescu <gxgpet@gmail.com>
2017-07-01 12:03:35 +0300

    Added CICacheAdapter, CIConfigurator, composer.json created.

---
+++ src/Phassets/CacheAdapters/CodeIgniterCacheAdapter.php
@@ -0,0 +1,59 @@
+<?php
+
+use Phassets\Interfaces\CacheAdapter;
+use Phassets\Interfaces\Configurator;
+
+class CodeIgniterCacheAdapter implements CacheAdapter
+{
+    /**
+     * @var int Number of seconds until a cache entry expires
+     */
+    private $ttl;
+
+    /**
+     * @var CI_Controller CodeIgniter instance
+     */
+    private $ci;
+
+    /**
+     * CacheAdapter constructor.
+     *
+     * @param Configurator $configurator Chosen and loaded Phassets configurator.
+     */
+    public function __construct(Configurator $configurator)
+    {
+        $this->ttl = $configurator->getConfig('cache', 'ttl');
+
+        $this->ci = &get_instance();
+    }
+
+    /**
+     * Save a value in cache for a giving key and return that value,
+     * or false if the saving fails.
+     *
+     * @param string $key
+     * @param mixed $value
+     * @param int $ttl Number of seconds before the value expires; not required
+     *                 since this can be retrieved from cache > ttl config.
+     * @return mixed|bool The saved value or false if saving cannot be performed
+     */
+    public function save($key, $value, $ttl = null)
+    {
+        $ttl = $ttl !== null ? $ttl : $this->ttl;
+
+        $success = $this->ci->cache->save($key, $value, $ttl);
+
+        return $success ? $value : false;
+    }
+
+    /**
+     * Return the cached value for a given key or false on failure.
+     *
+     * @param string $key
+     * @return mixed The cache value or false on failure
+     */
+    public function get($key)
+    {
+        return $this->ci->cache->get($key);
+    }
+}
---
+++ src/Phassets/Configurators/CodeIgniterConfigurator.php
@@ -0,0 +1,47 @@
+<?php
+
+use Phassets\Interfaces\Configurator;
+
+class CodeIgniterConfigurator implements Configurator
+{
+    /**
+     * The name of /application/config/ configuration file.
+     */
+    const CONFIG_FILE_NAME = 'phassets';
+
+    /**
+     * @var CI_Controller CodeIgniter instance
+     */
+    private $ci;
+
+    /**
+     * CodeIgniterConfigurator constructor.
+     */
+    public function __construct()
+    {
+        $this->ci = &get_instance();
+
+        $this->ci->config->load(self::CONFIG_FILE_NAME, false, true);
+    }
+
+    /**
+     * Returns the config item having a specific name; if that setting
+     * is an array, an index may be supplied in order to fetch the exact
+     * array element.
+     *
+     * @param string $name Setting name
+     * @param string $index If setting is an array, this can be array's key
+     *                      for proper element fetch, otherwise it should be null
+     * @return mixed The setting value; otherwise null
+     */
+    public function getConfig($name, $index = null)
+    {
+        if ($index === null) {
+            return $this->ci->config->item($name, 'phassets');
+        }
+
+        $setting = $this->ci->config->item($name, 'phassets');
+
+        return isset($setting[$index]) ? $setting[$index] : null;
+    }
+}
